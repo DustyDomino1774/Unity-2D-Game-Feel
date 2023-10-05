a77dc5c8795e3ba415637a6e0cc11c720d44551e
Dominic London <londonrdominic@gmail.com>
2023-10-05 14:29:19 -0700

    Observe Pattern Implement

--- Assets/Scripts/Gun/Gun.cs
+++ Assets/Scripts/Gun/Gun.cs
@@ -1,28 +1,63 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
+    public static Action OnShoot;
+
     public Transform BulletSpawnPoint => _bulletSpawnPoint;
 
     [SerializeField] private Transform _bulletSpawnPoint;
     [SerializeField] private Bullet _bulletPrefab;
+    [SerializeField] private float _gunFireCD = .5f;
+
+    private Vector2 _mousePos;
+    private float _lastFireTime = 0f;
     
     private void Update()
     {
         Shoot();
+        RotateGun();
+    }
+
+    private void OnEnable() 
+    {
+        OnShoot += ShootProjectile;
+        OnShoot += ResetLastFireTime;
+    }
+
+    private void OnDisable() 
+    {
+        OnShoot -= ShootProjectile;
+        OnShoot -= ResetLastFireTime;
     }
 
     private void Shoot()
     {
-        if (Input.GetMouseButtonDown(0)) {
-            ShootProjectile();
+        if (Input.GetMouseButton(0) && Time.time >= _lastFireTime) {
+           OnShoot?.Invoke(); // ? null conditional operator to fire only when subscribed to avoid null exception 
         }
     }
 
     private void ShootProjectile()
     {
         Bullet newBullet = Instantiate(_bulletPrefab, _bulletSpawnPoint.position, Quaternion.identity);
+        newBullet.Init(_bulletSpawnPoint.position, _mousePos);
     }
+
+    private void ResetLastFireTime() 
+    {
+        _lastFireTime = Time.time + _gunFireCD;
+    }
+
+    private void RotateGun() 
+    {
+        _mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = PlayerController.Instance.transform.InverseTransformPoint(_mousePos);
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.localRotation = Quaternion.Euler(0, 0, angle);
+    }
+
 }
