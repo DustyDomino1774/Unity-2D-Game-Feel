ca5a6a5841720e1a5d34cbb8b72a29e2930d7468
Dominic London <londonrdominic@gmail.com>
2023-10-26 23:54:45 -0700

    Hit Flash

--- Assets/Scripts/Gun/Bullet.cs
+++ Assets/Scripts/Gun/Bullet.cs
@@ -35,6 +35,10 @@ public class Bullet : MonoBehaviour
 
         Knockback knockback = other.gameObject.GetComponent<Knockback>();
         knockback?.GetKnockedBack(PlayerController.Instance.transform.position, _knockBackThrust);
-        _gun.ReleaseBulletFromPool(this);
+
+        Flash flash = other.gameObject.GetComponent<Flash>();
+        flash?.StartFlash();
+
+        _gun?.ReleaseBulletFromPool(this);
     }
 }
